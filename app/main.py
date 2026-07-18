34cab416d0b6501b177dfa1bbeb947ab6b429da8
Eklavya <ravikumardhal@gmail.com>
2026-07-19 01:33:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,9 +7,18 @@ from app.domains.auth import router as auth_router
 from app.domains.member import router as friend_router
 from app.domains.photos import router as photo_router
 from app.domains.chat import router as chat_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI();
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,        
+    allow_methods=["*"],               
+    allow_headers=["*"]
+)
 user_models.Base.metadata.create_all(bind = engine);
 photo_models.Base.metadata.create_all(bind = engine);
 friends_model.Base.metadata.create_all(bind = engine);
